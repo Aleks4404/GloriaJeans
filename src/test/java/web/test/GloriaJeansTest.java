e1eff93aab9e3f20a29ada704efe2690f00fa643
Aleksey Efimov <Aleks4404@mail.ru>
2021-10-20 06:50:31 +0400

    Added file GloriaJeansTest.java

--- src/test/java/web/test/GloriaJeansTest.java
+++ src/test/java/web/test/GloriaJeansTest.java
@@ -54,42 +54,42 @@ public class GloriaJeansTest {
         driver.findElement(By.cssSelector("p.caption-12.caption--underline.cursor-pointer.js-close-button")).click();
     }
 
-    @Test // TODO Ввести код из СМС и нажать Enter
-    @DisplayName("Регистрация с помощью номера телефона")
-    public void registrationUsingAPhoneNumber() {
-        driver.get("https://www.gloria-jeans.ru/");
-        driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
-        driver.findElement(By.xpath("//*[text()='Да, верно']")).click();
-        driver.findElement(By.cssSelector("div.header-body__account-button.js-active-pop-up-button.js-change-form-button.js-button-analytics-pop-up-open")).click();
-        driver.findElement(By.xpath("//div[15]/div/div/div[2]/div[2]")).click();
-        driver.findElement(By.xpath("//div/div[3]/div[2]/div/div[2]/div")).click();
-        driver.findElement(By.cssSelector("div.hide--no-hide-height.js-switchable-content.show.active > form.send-form.js-send-form > div.text-input.margin-bottom-16 > div.wrapper-text-zone.wrapper-text-zone--margin-bottom-0.js-text-zone > input[name=\"cellphone\"]")).sendKeys(DataHelper.generateNamberPhone());
-        driver.findElement(By.cssSelector("div.hide--no-hide-height.js-switchable-content.show.active > form.send-form.js-send-form > div.send-form__button-block > button.press-button.js-phone-test-button")).click();
-        driver.findElement(By.cssSelector("div.hide.js-code-block.hide--no-animate.js-info-text.show > div.text-input.js-code-input > div.wrapper-text-zone.js-text-zone > input[name=\"code\"]")).sendKeys("  ");
-        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-        driver.findElement(By.cssSelector("div.hide--no-hide-height.js-switchable-content.show.active > form.send-form.js-send-form > div.send-form__button-block > button.press-button.js-button-form")).click();
-        driver.findElement(By.cssSelector("div.wrapper-text-zone.js-text-zone > input[name=\"email\"]")).sendKeys(DataHelper.generateEmail());
-        driver.findElement(By.cssSelector("button.press-button.js-button-form")).click();
-        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-        driver.findElement(By.cssSelector("p.caption-24.caption--uppercase.caption--bold.margin-bottom-16"));
-        driver.findElement(By.cssSelector("p.caption-12.caption--underline.cursor-pointer.js-close-button")).click();
-    }
+//    @Test // TODO Ввести код из СМС и нажать Enter
+//    @DisplayName("Регистрация с помощью номера телефона")
+//    public void registrationUsingAPhoneNumber() {
+//        driver.get("https://www.gloria-jeans.ru/");
+//        driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+//        driver.findElement(By.xpath("//*[text()='Да, верно']")).click();
+//        driver.findElement(By.cssSelector("div.header-body__account-button.js-active-pop-up-button.js-change-form-button.js-button-analytics-pop-up-open")).click();
+//        driver.findElement(By.xpath("//div[15]/div/div/div[2]/div[2]")).click();
+//        driver.findElement(By.xpath("//div/div[3]/div[2]/div/div[2]/div")).click();
+//        driver.findElement(By.cssSelector("div.hide--no-hide-height.js-switchable-content.show.active > form.send-form.js-send-form > div.text-input.margin-bottom-16 > div.wrapper-text-zone.wrapper-text-zone--margin-bottom-0.js-text-zone > input[name=\"cellphone\"]")).sendKeys(DataHelper.generateNamberPhone());
+//        driver.findElement(By.cssSelector("div.hide--no-hide-height.js-switchable-content.show.active > form.send-form.js-send-form > div.send-form__button-block > button.press-button.js-phone-test-button")).click();
+//        driver.findElement(By.cssSelector("div.hide.js-code-block.hide--no-animate.js-info-text.show > div.text-input.js-code-input > div.wrapper-text-zone.js-text-zone > input[name=\"code\"]")).sendKeys("  ");
+//        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+//        driver.findElement(By.cssSelector("div.hide--no-hide-height.js-switchable-content.show.active > form.send-form.js-send-form > div.send-form__button-block > button.press-button.js-button-form")).click();
+//        driver.findElement(By.cssSelector("div.wrapper-text-zone.js-text-zone > input[name=\"email\"]")).sendKeys(DataHelper.generateEmail());
+//        driver.findElement(By.cssSelector("button.press-button.js-button-form")).click();
+//        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+//        driver.findElement(By.cssSelector("p.caption-24.caption--uppercase.caption--bold.margin-bottom-16"));
+//        driver.findElement(By.cssSelector("p.caption-12.caption--underline.cursor-pointer.js-close-button")).click();
+//    }
 
-    @Test // TODO Ввести код из СМС и нажать Enter
-    @DisplayName("Авторизация с помощью номера телефона")
-    public void authorizationUsinAaPhoneNumber() throws Exception {
-        driver.get("https://www.gloria-jeans.ru/");
-        driver.findElement(By.xpath("//*[text()='Да, верно']")).click();
-        driver.findElement(By.cssSelector("span.caption-14.margin-left-8")).click();
-        driver.findElement(By.xpath("//div/div[3]/div/div/div[2]/div")).click();
-        driver.findElement(By.name("cellphone")).click();
-        driver.findElement(By.name("cellphone")).clear();
-        driver.findElement(By.name("cellphone")).sendKeys(DataHelper.generateNamberPhone());
-        driver.findElement(By.cssSelector("button.press-button.js-phone-test-button")).click();
-        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-        driver.findElement(By.cssSelector("div.send-form__button-block > button.press-button.js-button-form")).click();
-        driver.findElement(By.cssSelector("span.caption-14.margin-left-8.max-width-115.width-limit-ellipsis.js-customer-name-block")).click();
-  }
+//    @Test // TODO Ввести код из СМС и нажать Enter
+//    @DisplayName("Авторизация с помощью номера телефона")
+//    public void authorizationUsinAaPhoneNumber() throws Exception {
+//        driver.get("https://www.gloria-jeans.ru/");
+//        driver.findElement(By.xpath("//*[text()='Да, верно']")).click();
+//        driver.findElement(By.cssSelector("span.caption-14.margin-left-8")).click();
+//        driver.findElement(By.xpath("//div/div[3]/div/div/div[2]/div")).click();
+//        driver.findElement(By.name("cellphone")).click();
+//        driver.findElement(By.name("cellphone")).clear();
+//        driver.findElement(By.name("cellphone")).sendKeys(DataHelper.generateNamberPhone());
+//        driver.findElement(By.cssSelector("button.press-button.js-phone-test-button")).click();
+//        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+//        driver.findElement(By.cssSelector("div.send-form__button-block > button.press-button.js-button-form")).click();
+//        driver.findElement(By.cssSelector("span.caption-14.margin-left-8.max-width-115.width-limit-ellipsis.js-customer-name-block")).click();
+//  }
 
     @Test
     @DisplayName("Авторизация с помощью Email")
